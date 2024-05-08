30f10b449de3aec1498e25560cd4eeacae6b53c2
Shunsuke-1994 <oolongtea1980@gmail.com>
2024-05-08 23:54:31 +0900

    changed dirs

---
+++ scripts/eval_scores.py
@@ -0,0 +1,16 @@
+# script for eval of potential and confidence scores
+# run on a batch of pdbs
+import sys
+import os
+import argparse
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from src.score import RASP
+
+def main():
+    return 
+
+
+if __name__ == '__main__':
+    main()
+    
+
