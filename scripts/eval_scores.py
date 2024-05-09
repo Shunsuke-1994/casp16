37d515f63eb980dd195c90a38726b7a9e6063e2e
Shunsuke-1994 <oolongtea1980@gmail.com>
2024-05-09 20:43:55 +0900

    update cgRNASP

--- scripts/eval_scores.py
+++ scripts/eval_scores.py
@@ -3,13 +3,23 @@
 import sys
 import os
 import argparse
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from src.score import DFIER_RNA, RNA_BRiQ, rsRNASP, cgRNASP
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from src.score import cgRNASP
 
 def main():
     parser = argparse.ArgumentParser(description='Evaluate potential scores')
-    parser.add_argument('pdb_dir', help='directory of pdb files')
-    parser.add_argument('out_dir', help='output directory')
+    parser.add_argument('--pdb_dir', help='directory of pdb files')
+    parser.add_argument('--out_dir', help='output directory')
+    parser.add_argument('--print', help='print output', action='store_true')
+    args = parser.parse_args()
+
+    res_cgrnasp = cgRNASP.cgRNASP(args.pdb_dir, args.out_dir)
+    res_cgrnasp_p = cgRNASP.cgRNASP_C(args.pdb_dir, args.out_dir)
+    res_cgrnasp_pc = cgRNASP.cgRNASP_PC(args.pdb_dir, args.out_dir)
+    if args.print:
+        print(res_cgrnasp)
+        print(res_cgrnasp_p)
+        print(res_cgrnasp_pc)
     return 
 
 
